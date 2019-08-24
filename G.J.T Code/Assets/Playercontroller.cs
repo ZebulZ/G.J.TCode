7e9a2cdbfc384cd2f265c8066a0105ed566f5d84
babyfireyt <babyfireyt@gmail.com>
2019-08-24 23:19:33 +0200

    Made jumping better

--- G.J.T Code/Assets/Playercontroller.cs
+++ G.J.T Code/Assets/Playercontroller.cs
@@ -6,13 +6,13 @@ public class Playercontroller : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float JumpPower;
     [SerializeField] private Rigidbody2D rb;
-    [HideInInspector] public bool Grounded = false;
+    public bool Grounded = false;
     [SerializeField] private Transform HitPoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -35,14 +35,12 @@ public class Playercontroller : MonoBehaviour
     }
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(new Vector2(HitPoint.position.x,HitPoint.position.y), -Vector2.up);
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(HitPoint.position.x, HitPoint.position.y), -Vector2.up);
         if (hit)
         {
             float distance = Mathf.Abs(hit.point.y - transform.position.y);
 
-            Debug.Log(distance);
-
-            if(distance < 0.1)
+            if (distance < 2.9)
             {
                 Grounded = true;
             }
