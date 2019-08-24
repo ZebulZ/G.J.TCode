b4ccbb09bea991cc7286b348ff1a9d92d24ad506
babyfireyt <babyfireyt@gmail.com>
2019-08-24 23:30:15 +0200

    Update Playercontroller.cs

--- G.J.T Code/Assets/Playercontroller.cs
+++ G.J.T Code/Assets/Playercontroller.cs
@@ -3,21 +3,24 @@
 public class Playercontroller : MonoBehaviour
 
 {
+    [Header("GameChangingFloats")]
+
     [SerializeField] private float speed;
     [SerializeField] private float JumpPower;
+
+    [Header("Requirements")]
+
     [SerializeField] private Rigidbody2D rb;
-    public bool Grounded = false;
     [SerializeField] private Transform HitPoint;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    [Header("Bools")]
 
-    }
+    public bool Grounded = false;
 
-    // Update is called once per frame
+    //the movement is calculated here
     void FixedUpdate()
     {
+        //Simple Movement
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(1 * speed * Time.deltaTime, 0, 0);
@@ -26,20 +29,15 @@ public class Playercontroller : MonoBehaviour
         {
             transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
         }
-
-        if (Input.GetKey(KeyCode.W) && Grounded)
-        {
-            Vector2 Jump = new Vector2(0, 50 * JumpPower);
-            rb.AddForce(Jump);
-        }
-    }
-    void Update()
-    {
+        //Trow ray towards the ground
         RaycastHit2D hit = Physics2D.Raycast(new Vector2(HitPoint.position.x, HitPoint.position.y), -Vector2.up);
+        //if hit something
         if (hit)
         {
+            //get distance of the ground
             float distance = Mathf.Abs(hit.point.y - transform.position.y);
 
+            //if touching the ground (meanning that the ray is 2.9 units or less long, this may vary) Grounded = true if not Grounded = false
             if (distance < 2.9)
             {
                 Grounded = true;
@@ -48,6 +46,13 @@ public class Playercontroller : MonoBehaviour
             {
                 Grounded = false;
             }
+        }
+        //if press W and touching the floor 
+        if (Input.GetKey(KeyCode.W) && Grounded)
+        {
+            //Add 2d force up
+            Vector2 Jump = new Vector2(0, 50 * JumpPower);
+            rb.AddForce(Jump);
         }
     }
 }
