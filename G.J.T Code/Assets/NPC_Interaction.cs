1badcc396e52b0954d15739dee2975625bcb9c3f
babyfireyt <babyfireyt@gmail.com>
2019-08-26 21:36:53 +0200

    npc added

---
+++ G.J.T Code/Assets/NPC_Interaction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class NPC_Interaction : MonoBehaviour
+{
+    [SerializeField] private Collider2D col;
+
+    private void OnTriggerStay(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("Player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
