4c52fb48f1e6b10d9e3e0fd33e225783b3366848
babyfireyt <babyfireyt@gmail.com>
2019-08-27 16:19:32 +0200

    Finnished NPC preview

--- G.J.T Code/Assets/NPC_Interaction.cs
+++ G.J.T Code/Assets/NPC_Interaction.cs
@@ -4,15 +4,32 @@ using TMPro;
 
 public class NPC_Interaction : MonoBehaviour
 {
-    [SerializeField] private Collider2D col;
-    [SerializeField] private TMP_Text Text;
-    [SerializeField] private GameObject TextFied;
     [SerializeField] private Animator Anim;
 
+    [Header("TextReferences")]
+    [SerializeField] private TMP_Text ButtonPositive;
+    [SerializeField] private TMP_Text ButtonNegative;
+    [SerializeField] private TMP_Text Text;
+
+    [Header("Enable/Disable")]
+    [SerializeField] private GameObject TextFied;
+    [SerializeField] private GameObject ItemDisplay;
+    [SerializeField] private GameObject Button1;
+    [SerializeField] private GameObject Button2;
+
+    [Header("Customizable Text")]
     [TextArea]
     public string[] Dialogues;
     [TextArea]
     public string Nodialog;
+
+    [Header("Default text of yes, no")]
+    [SerializeField] private string BuyPositiveDef;
+    [SerializeField] private string BuyNegativeDef;
+
+    [Header("Buy Buttons")]
+    [SerializeField] private string BuyPositive;
+    [SerializeField] private string BuyNegative;
 
     private int DialogueCount = 0;
 
@@ -30,38 +47,44 @@ public class NPC_Interaction : MonoBehaviour
             TextFied.SetActive(true);
             //equal the text to the next dialogue
             Text.text = Dialogues[0];
+            ItemDisplay.SetActive(false);
+            Button1.SetActive(true);
+            Button2.SetActive(true);
         }
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            if(DialogueCount > Dialogues.Length)
-            {
-                DialogueCount = Dialogues.Length;
-            }
-        }
-    }
-
-   public void Yes()
+    public void Yes()
     {
         DialogueCount++;
         Text.text = Dialogues[DialogueCount];
+        if(DialogueCount == Dialogues.Length -1)
+        {
+            ItemDisplay.SetActive(true);
+            ButtonNegative.text = BuyNegative;
+            ButtonPositive.text = BuyPositive;
+        }
     }
 
    public void No()
     {
         Text.text = Nodialog;
+        ItemDisplay.SetActive(false);
+        Button1.SetActive(false);
+        Button2.SetActive(false);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            DialogueCount = 0;
+            ItemDisplay.SetActive(false);
+
+            ButtonNegative.text = BuyNegativeDef;
+            ButtonPositive.text = BuyPositiveDef;
+
             Anim.SetBool("Enter", false);
             StartCoroutine(WaitForTransition());
-            DialogueCount = 0;
         }
     }
 
