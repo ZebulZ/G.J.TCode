946be5c3b146e92521a944eb5cbdfcc9ec87e0fc
babyfireyt <babyfireyt@gmail.com>
2019-08-27 20:42:13 +0200

    shooting??

---
+++ G.J.T Code/Assets/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // [SerializeField] private float Damage;
+    [SerializeField] private float Speed;
+    [SerializeField] private Vector2 Direction;
+    [SerializeField] private float LifeTime = 2;
+
+    private void OnEnable()
+    {
+        Direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Destroy(gameObject, LifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Direction * Time.fixedDeltaTime * Speed);
+    }
+}
---
+++ G.J.T Code/Assets/Shoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] private GameObject Bullet;
+    [SerializeField] private float FireRate;
+    //position of the bullet spawn
+    [SerializeField] private Transform BulletPoint;
+
+    private bool CanShoot = true;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && CanShoot)
+        {
+            Instantiate(Bullet, BulletPoint);
+            CanShoot = false;
+            StartCoroutine(firerate());
+        }
+    }
+
+    IEnumerator firerate()
+    {
+        yield return new WaitForSeconds(FireRate);
+        CanShoot = true;
+    }
+}
