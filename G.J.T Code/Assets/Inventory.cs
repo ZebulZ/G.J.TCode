52c9a13a2951e22923eaf0fc98c61c6f7fed41b4
babyfireyt <babyfireyt@gmail.com>
2019-08-28 20:34:18 +0200

    added simple health
    enemy and player health, also the enemy can recieve damage

---
+++ G.J.T Code/Assets/Inventory.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public GameObject inventory;
+    public bool inventoryEnabled;
+
+    public int allSlots;
+    public int enabledSlots;
+    private GameObject[] slot;
+    public GameObject slotHolder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        allSlots = 5;
+        slot = new GameObject[allSlots];
+        for (int i = 0; i < allSlots; i++)
+        {
+            slot[i] = slotHolder.transform.GetChild(i).gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryEnabled = !inventoryEnabled;
+        }
+
+        if (inventoryEnabled)
+        {
+            inventory.SetActive(true);
+        }
+        else
+        {
+            inventory.SetActive(false);
+        }
+    }
+}
